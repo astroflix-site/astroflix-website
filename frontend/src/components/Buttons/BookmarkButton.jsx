033974f0a8d721c421779683d027b9579e59825d
bretc9 <dasa68952@gmail.com>
2024-11-17 00:33:12 +0530

    Added Bookmark

--- frontend/src/components/Buttons/BookmarkButton.jsx
+++ frontend/src/components/Buttons/BookmarkButton.jsx
@@ -35,7 +35,7 @@ const BookmarkButton = ({ contentId }) => {
         <button
             onClick={handleBookmark}
             className={`px-4  py-2 rounded-lg text-white transition-all duration-200
-                ${isBookmarked ? 'bg-slate-100 hover:bg-slate-300' : 'bg-neutral-500 hover:bg-neutral-900'}`}
+                ${isBookmarked ? 'bg-slate-700 hover:bg-slate-600' : 'bg-neutral-500 hover:bg-neutral-900'}`}
         >
             {isBookmarked ? <FaBookmark /> : <FaRegBookmark />} {/* Using react-icons */}
         </button>
