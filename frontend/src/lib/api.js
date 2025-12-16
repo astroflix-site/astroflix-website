721238b1ee6f4a291430ff6b92e79875cab1b69d
Zer0Rea1 <notrana1122@outlook.com>
2025-12-16 18:23:00 +0500

    bluh bluh

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-// const API_URL = "http://localhost:5000/api";
+const API_URL = "https://astro-flix.netlify.app/api";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: API_URL,
     withCredentials: true,
 });
 
