3ebd18a77394bcb771dc961af69d6e328df3f69f
Chandra <dasa68952@gmail.com>
2025-12-18 16:47:23 +0530

    Updated Cors to allow requests from Mobile App

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,44 @@
-const express = require('express')
-const app = express()
-require("dotenv").config()
-const pool = require('./db') // Import PostgreSQL pool
-const userApi = require('./api/user')
-const cookieParser = require('cookie-parser')
-app.use(express.json());
-app.use(cookieParser());
-const cors = require('cors');
-const contentApi = require('./api/content')
-app.use(cors({
-    origin: [
-        'https://astroflix-website.vercel.app',
-        'http://localhost:5173'
-    ], // Frontend origins
-    credentials: true, // Allow credentials (cookies) to be sent
-
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-//all routes
-app.use('/api', userApi)
-app.use('/api', contentApi)
-const bookmarkApi = require('./api/bookmark')
-app.use('/api', bookmarkApi)
-app.get('/',)
-
-app.listen(process.env.PORT, () => {
-    console.log('Server Started On ' + process.env.PORT)
-})
-
-
-module.exports = app;
+1: const express = require('express')
+2: const app = express()
+3: require("dotenv").config()
+4: const pool = require('./db') // Import PostgreSQL pool
+5: const userApi = require('./api/user')
+6: const cookieParser = require('cookie-parser')
+7: app.use(express.json());
+8: app.use(cookieParser());
+9: const cors = require('cors');
+10: const contentApi = require('./api/content')
+11: app.use(cors({
+12:   origin: (origin, callback) => {
+13:     // Allow requests with no origin (React Native, Postman, mobile apps)
+14:     if (!origin) return callback(null, true);
+15: 
+16:     const allowedOrigins = [
+17:       'https://astroflix-website.vercel.app',
+18:       'http://localhost:5173'
+19:     ];
+20: 
+21:     if (allowedOrigins.includes(origin)) {
+22:       return callback(null, true);
+23:     }
+24: 
+25:     return callback(new Error('Not allowed by CORS'));
+26:   },
+27:   credentials: true,
+28:   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+29:   allowedHeaders: ['Content-Type', 'Authorization']
+30: }));
+31: 
+32: //all routes
+33: app.use('/api', userApi)
+34: app.use('/api', contentApi)
+35: const bookmarkApi = require('./api/bookmark')
+36: app.use('/api', bookmarkApi)
+37: app.get('/',)
+38: 
+39: app.listen(process.env.PORT, () => {
+40:     console.log('Server Started On ' + process.env.PORT)
+41: })
+42: 
+43: 
+44: module.exports = app;
